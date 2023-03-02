ef2f8a465a92d74818254f1e700d2ae3b9e13439
yunayoun <nsook91@naver.com>
2023-03-02 18:19:21 +0900

    coin

--- React/beginners/src/App.js
+++ React/beginners/src/App.js
@@ -1,37 +1,30 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App() {
-  const [toDo,setToDo] = useState('');
-  const [toDos,setToDos] = useState([]);
-  const onChange = (e)=>{setToDo(e.target.value)}
-  const onSubmit = (e)=>{
-    e.preventDefault();
-    if(toDo===''){
-      return;
-    }
-    setToDos((currentArr)=>[toDo, ...currentArr]);
-    setToDo('');
-  }
-
+  const [loading,setLoading] = useState(true);
+  const [coins,setCoins] = useState([]);
+  useEffect(()=>{
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response)=>response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  },[]);
   return (
     <div>
-      <h1>MY TO DO({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={toDo} 
-          onChange={onChange} 
-          type='text' 
-          placeholder="write..."
-        ></input>
-        <button >add to do</button>
-      </form>
-      <hr/>
-      <ul>
-        {/* {toDos.map((item)=> <li >{item}</li>)} 오류뜸.key 필요 */}
-        {toDos.map((item,index)=> <li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? 
+        (<strong>Loading..</strong>) : (
+      <select>
+        {coins.map((coin)=>
+          <option>
+            {coin.name} {coin.symbol} : ${coin.quotes.USD.price}USD
+          </option>
+          )}
+        </select>
+        )}
     </div>
   );
 }
-
 export default App;
