4711c159ef803f86b9367269e11d902465d4e813
yunayoun <nsook91@naver.com>
2023-03-02 13:16:48 +0900

    useEffect

--- React/beginners/src/App.js
+++ React/beginners/src/App.js
@@ -1,11 +1,35 @@
+import { useState,useEffect } from "react";
 
 
 function App() {
+  const [counter,setValue] = useState(0);
+  const [keyword,setKeyword] = useState('');
+  const onClick = ()=> setValue((prev)=>prev+1);
+  const onChange = (e)=>setKeyword(e.target.value);
+
+  useEffect(()=>{
+    console.log('only once')
+  },[]);
+  useEffect(()=>{
+    console.log('keyword change')
+  },[keyword]);
+  useEffect(()=>{
+    console.log('counter&&keyword change')
+  },[counter,keyword]);
+
   return (
     <div>
-    <h1>하하하하하ㅏㅎ</h1>
+    <input 
+    value = {keyword} 
+    onChange={onChange} 
+    placeholder='write' 
+    type='text'
+    ></input>
+    <h1>{counter}</h1>
+    <button onClick = {onClick}>click me</button>
     </div>
   );
 }
 
 export default App;
+//useEffect가 두번반복된이유는 index.js에서 검사하는태그때문.
